19585e3f31b9f57a240199cf673f7fcf27625167
Xuenan Xu <wsntxxn@gmail.com>
2020-12-07 20:03:47 +0800

    Add README

---
+++ README.md
@@ -0,0 +1,43 @@
+# Text-to-Audio Grounding
+
+This repository provides the data and source code for Text-to-Audio Grounding (TAG) task.
+
+## Dataset
+
+### Audio
+
+The dataset in this repository is an augmented audio captioning dataset. It is based on [Audiocaps](https://www.aclweb.org/anthology/N19-1011.pdf), which is established using part of a audio event dataset, [AudioSet](https://research.google.com/audioset). Therefore, audio files can be downloaded from [AudioSet](https://research.google.com/audioset/download.html). 
+
+### Label
+
+The labels for TAG are provided in `data/*.json`. The json file can be read by `pandas`. Each file includes these columns:
+* audiocap_id: `id` used in the [Audiocaps](https://github.com/cdjkim/audiocaps/tree/master/dataset) label file
+* filename: raw wave filename, in the form of `[youtube_id]_[start_time]_[end_time].wav`, you can use this to grab the file from Youtube
+* caption: audio caption provided by Audiocaps
+* tokens: tokenized audio caption
+* soundtag: phrase representing sound which is extracted from `caption`
+* start_word: the index of the first word of `soundtag`, starting from 0 (the index of the first token in `tokens` is 0)
+* timestamps: a list of starting and ending timestamps of the corresponding `soundtag`; for example, if the `soundtag` is "a dog is barking" and `timestamps` is [[1, 3], [7, 8]], it means the dog barks twice in the audio: the first bark continues from 1s to 3s and the second from 7s to 8s
+
+## TAG baseline
+
+We provide a baseline approach for TAG in this repository. To run the baseline, complete the following steps:
+1. download the raw audio files and rename them in order that your computer can find the audio files via `filename` in `data/*.json` (this also requires a little processing).
+2. checkout the code and install the required python packages:
+```bash
+git clone https://github.com/wsntxxn/TextToAudioGrounding
+pip install -r requirements.txt
+```
+3. extract audio feature using `utils/extract_feature.py`.
+4. modify the configuration file `config/conf.yaml` and run the training command:
+```bash
+python run.py train config/conf.yaml
+```
+5. evaluate the trained model:
+```bash
+export EXP_PATH=***
+export AUDIO_FEATURE=***
+export LABEL_FILE=data/annotation.test.json
+python run.py evaluate $EXP_PATH $AUDIO_FEATURE $LABEL_FILE
+```
+where `EXP_PATH` and `AUDIO_FEATURE` are the experiment checkpoint directory and audio feature file corresponding to your configuration.
---
+++ utils/train_util.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import os
+import sys
+import logging
+import datetime
+import yaml
+import torch
+import numpy as np
+import pandas as pd
+from pprint import pformat
+
+def genlogger(outputfile, level="INFO"):
+    formatter = logging.Formatter(
+        "[ %(levelname)s : %(asctime)s ] - %(message)s")
+    logger = logging.getLogger(__name__ + "." + outputfile)
+    logger.setLevel(getattr(logging, level))
+    # Log results to std
+    # stdhandler = logging.StreamHandler(sys.stdout)
+    # stdhandler.setFormatter(formatter)
+    # Dump log to file
+    filehandler = logging.FileHandler(outputfile)
+    filehandler.setFormatter(formatter)
+    logger.addHandler(filehandler)
+    # logger.addHandler(stdhandler)
+    return logger
+
+
+def pprint_dict(in_dict, outputfun=sys.stdout.write, formatter='yaml'):
+    """pprint_dict
+
+    :param outputfun: function to use, defaults to sys.stdout
+    :param in_dict: dict to print
+    """
+    if formatter == 'yaml':
+        format_fun = yaml.dump
+    elif formatter == 'pretty':
+        format_fun = pformat
+    for line in format_fun(in_dict).split('\n'):
+        outputfun(line)
+
+
+def encode_labels(labels: pd.Series, encoder=None):
+    """encode_labels
+
+    Encodes labels
+
+    :param labels: pd.Series representing the raw labels e.g., Speech, Water
+    :param encoder (optional): Encoder already fitted 
+    returns encoded labels (one hot) and the encoder
+    """
+    import sklearn.preprocessing as pre
+
+    assert isinstance(labels, pd.Series), "Labels need to series"
+    if not encoder:
+        encoder = pre.LabelEncoder()
+        encoder.fit(labels)
+    labels_encoded = encoder.transform(labels)
+    return labels_encoded.tolist(), encoder
+
+
+def parse_config_or_kwargs(config_file, **kwargs):
+    with open(config_file) as con_read:
+        yaml_config = yaml.load(con_read, Loader=yaml.FullLoader)
+    # passed kwargs will override yaml config
+    return dict(yaml_config, **kwargs)
+
+
+# def parse_augments(augment_list):
+    # """parse_transforms
+    # parses the transformation string in configuration file to corresponding methods
+
+    # :param transform_list: list
+    # """
+    # from datasets import augment
+
+    # augments = []
+    # for transform in augment_list:
+        # if transform == "timemask":
+            # augments.append(augment.TimeMask(1, 50))
+        # elif transform == "freqmask":
+            # augments.append(augment.FreqMask(1, 10))
+    # return torch.nn.Sequential(*augments)
+
+
+def criterion_improver(mode):
+    assert mode in ("loss", "acc", "score", "F1")
+    best_value = np.inf if mode == "loss" else 0
+
+    def comparator(x, best_x):
+        return x < best_x if mode == "loss" else x > best_x
+
+    def inner(x):
+        nonlocal best_value
+
+        if comparator(x, best_value):
+            best_value = x
+            return True
+        return False
+    return inner
+
+
+def log_results(engine,
+                cv_evaluator, 
+                cv_dataloader, 
+                outputfun=sys.stdout.write,
+                train_metrics=["loss", "accuracy"], 
+                cv_metrics=["loss", "accuracy"]):
+    train_results = engine.state.metrics
+    cv_evaluator.run(cv_dataloader)
+    cv_results = cv_evaluator.state.metrics
+    output_str_list = [
+        "Validation Results - Epoch : {:<4}".format(engine.state.epoch)
+    ]
+    for metric in train_metrics:
+        output = train_results[metric]
+        if isinstance(output, torch.Tensor):
+            output = output.item()
+        output_str_list.append("{} {:<5.2g} ".format(
+            metric, output))
+    for metric in cv_metrics:
+        output = cv_results[metric]
+        if isinstance(output, torch.Tensor):
+            output = output.item()
+        output_str_list.append("{} {:5<.2g} ".format(
+            metric, output))
+
+    outputfun(" ".join(output_str_list))
+
+
+def save_model_on_improved(engine,
+                           criterion_improved, 
+                           metric_key,
+                           dump,
+                           save_path):
+    if criterion_improved(engine.state.metrics[metric_key]):
+        torch.save(dump, save_path)
+
+
+def update_reduce_on_plateau(engine, scheduler, metric):
+    cv_loss = engine.state.metrics[metric]
+    scheduler.step(cv_loss)
